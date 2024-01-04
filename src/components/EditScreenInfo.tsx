e6ae003b7cd189451af8621a1ef46498008ac4c0
matsu3m <105341123+matsu3m@users.noreply.github.com>
2024-01-04 11:27:13 +0000

    Add unit test workflow

--- src/components/EditScreenInfo.tsx
+++ src/components/EditScreenInfo.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { StyleSheet } from "react-native";
 
 import Colors from "@/constants/Colors";
-import { ExternalLink } from "./ExternalLink";
-import { MonoText } from "./StyledText";
-import { Text, View } from "./Themed";
+import { ExternalLink } from "@/components/ExternalLink";
+import { MonoText } from "@/components//StyledText";
+import { Text, View } from "@/components//Themed";
 
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
